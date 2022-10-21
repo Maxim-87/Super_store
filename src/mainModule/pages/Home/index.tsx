bf38346e33aa763c5fd3504f30888118166a652a
Maxim <maksboks87@mail.ru>
2022-10-21 10:14:12 +0600

    add prettier add eslint plugins

--- src/mainModule/pages/Home/index.tsx
+++ src/mainModule/pages/Home/index.tsx
@@ -1,50 +1,61 @@
-import React, {useEffect} from 'react';
-import {Product} from 'base/components/Product';
-import {useDispatch, useSelector} from 'react-redux';
+import React, { useEffect } from 'react';
+
 import hash from 'object-hash';
-import {useNavigate} from 'react-router-dom';
-import mainModuleRoutes from 'base/constants/routes/mainModuleRoutes';
-import {InputSearch} from 'base/components/InputSearch/InputSearch';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import styles from './Home.module.scss';
+
+import { InputSearch } from 'base/components/InputSearch/InputSearch';
 import LoaderSpinner from 'base/components/LoaderSpinner';
-import {getProductsAction} from 'base/store/Products/actions';
-import styles from "./Home.module.scss";
+import { Product } from 'base/components/Product';
+import mainModuleRoutes from 'base/constants/routes/mainModuleRoutes';
+import { getProductsAction } from 'base/store/Products/actions';
 
-
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const Home = () => {
   const dispatch = useDispatch();
-  const products: any = useSelector<any>(state => state.products)
-  const isLoading: any = useSelector<any>(state => state.products.products.isLoading)
+  const products: any = useSelector<any>((state) => state.products);
+  const isLoading: any = useSelector<any>((state) => state.products.products.isLoading);
   const history = useNavigate();
 
   useEffect(() => {
-    if(products.data) {
-      return
+    if (products.data) {
+      return;
     }
     dispatch(getProductsAction());
-  }, [])
+  }, [products.data, dispatch]);
 
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   const redirectToAddProductPageHandler = () => {
-    history(mainModuleRoutes.create.root)
-  }
+    history(mainModuleRoutes.create.root);
+  };
 
   return (
     <div className={styles.home}>
-      {isLoading ? <LoaderSpinner/> : <>
-        <input type="text"/>
-        <h2>Товары</h2>
-        <button onClick={redirectToAddProductPageHandler}> Добавить товар </button>
-        <InputSearch />
-        {/*<div className={styles.title}>*/}
-        {/*  <h3>Название</h3>*/}
-        {/*  <h3>Статус</h3>*/}
-        {/*  <h3>Цена</h3>*/}
-        {/*</div>*/}
-        <div className={styles.prod}>
-          {products?.products?.data?.map((product: any) => (
-            <Product product={product} key={hash(product)}/>
-          ))}
-        </div>
-      </>}
+      {isLoading ? (
+        <LoaderSpinner />
+      ) : (
+        <>
+          <input type="text" />
+          <h2>Товары</h2>
+          <button type="button" onClick={redirectToAddProductPageHandler}>
+            {' '}
+            Добавить товар{' '}
+          </button>
+          <InputSearch />
+          {/* <div className={styles.title}> */}
+          {/*  <h3>Название</h3> */}
+          {/*  <h3>Статус</h3> */}
+          {/*  <h3>Цена</h3> */}
+          {/* </div> */}
+          <div className={styles.prod}>
+            {products?.products?.data?.map((product: any) => (
+              <Product product={product} key={hash(product)} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/starter/store/store.ts
+++ src/starter/store/store.ts
@@ -1,30 +1,29 @@
-import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
-import {searchReducer} from "../../base/store/Search/reducer";
-import {productReducer} from "../../base/store/Product/reducer";
-import {productsReducer} from "../../base/store/Products/reducer";
-import rootSaga from "./rootSaga";
+import rootSaga from './rootSaga';
+
+import { productReducer } from 'base/store/Product/reducer';
+import { productsReducer } from 'base/store/Products/reducer';
+import { searchReducer } from 'base/store/Search/reducer';
 
 const rootReducer = combineReducers({
- products: productsReducer,
- product: productReducer,
- search: searchReducer,
-})
+  products: productsReducer,
+  product: productReducer,
+  search: searchReducer,
+});
 
 const sagaMiddleware = createSagaMiddleware();
 
 // @ts-ignore
-const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
+const composeEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as typeof compose) || compose;
 
-const configureStore = (preloadedState: any) => createStore(
-  rootReducer,
-  preloadedState,
-  composeEnhancers(applyMiddleware(sagaMiddleware)),
-);
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const configureStore = (preloadedState: any) =>
+  createStore(rootReducer, preloadedState, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 const store = configureStore({});
-sagaMiddleware.run(rootSaga);  //----must before store
+
+sagaMiddleware.run(rootSaga); // ----must before store
 
 export default store;
-
